115a0a7616ab12a1d4768fe7032ad280b5de08a6
Nevine Achola <nevine.achola@student.moringaschool.com>
2023-03-19 23:00:01 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+    input.addEventListener('click', function() {
+      alert('Yeah here we are again');
+      alert('I bet you just clicked me');
+
+    });
 }
+ 
